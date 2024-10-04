d8cf1de2f61350b313470b754a3d790d6b781fbe
225sumit <225sumitgupta@gmail.com>
2024-10-04 23:25:00 +0530

    not fixed bug

---
+++ avg.java
@@ -0,0 +1,17 @@
+public class avg {
+    public static void main(String[] args) {
+        Scanner in = new Scanner();
+        float averg,a,b,c,d;
+        System.out.println("Enter the first number:");
+        a=in.nextFloat();
+        System.out.println("Enter the second number:");
+        b=in.nextFloat();
+        System.out.println("Enter the second number:");
+        c=in.nextFloat();
+        System.out.println("Enter the second number:");
+        d=in.nextFloat();
+        averg=(a+b+c+d)/4;
+     
+       System.out.println("Average of the numbers is "+averg);
+    
+}
